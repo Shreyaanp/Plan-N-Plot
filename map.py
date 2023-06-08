9c02560b6a3e57eb98eaf6ffaffc87196a648e38
Shreyaan Pradhan <shreyaan.pradhan2020@vitstudent.ac.in>
2023-06-08 10:01:45 +0530

    little work

--- map.py
+++ map.py
@@ -6,6 +6,7 @@ import subprocess
 root = Tk()
 root.title('Select Area')
 root.geometry("900x700")
+root.configure(bg="black")  # Set the background color of the root window to black
 
 def capture_map():
     x = root.winfo_rootx() + my_label.winfo_x() + map_widget.winfo_x()
@@ -19,6 +20,7 @@ def capture_map():
 
 my_label = LabelFrame(root)
 my_label.pack(pady=20)
+my_label.configure(bg="black")  # Set the background color of the label frame to black
 
 map_widget = tkintermapview.TkinterMapView(my_label, width=800, height=600, corner_radius=0)
 map_widget.set_position(12.9165, 79.1325)
@@ -26,6 +28,6 @@ map_widget.set_zoom(12)
 map_widget.pack(side="left")
 
 capture_button = Button(root, text="Capture", command=capture_map, width=15, font=("Arial", 14))
-capture_button.pack(side="left", padx=10)
+capture_button.pack(side="right", padx=90)
 
 root.mainloop()
